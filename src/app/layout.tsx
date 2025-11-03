95d98effc7329179e8f0241b3238fb76f87118f4
Yure Silva <yuresilvaprof@gmail.com>
2025-11-02 21:27:20 -0300

    fix: Corrigindo footer da main

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { Header } from './components/layout/Header';
-import { Footer } from './components/layout/Footer';
+import { Footer } from './components/layout/footer';
 
 
 const inter = Inter({
